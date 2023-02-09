f2cd7fe6d1429ccaf6f103721e9247d135475e63
Nicolás Aguerre <naguerre@fi.uba.ar>
2023-02-08 23:59:44 -0300

    Adds booking cancellation modal

---
+++ src/components/CancelBookingModal.tsx
@@ -0,0 +1,74 @@
+import {
+  Button,
+  Modal,
+  ModalCloseButton,
+  ModalContent,
+  ModalOverlay,
+  Text,
+} from '@chakra-ui/react';
+import { graphql, useFragment } from 'react-relay';
+import { CancelBookingModalFragment$key } from './__generated__/CancelBookingModalFragment.graphql';
+import styles from '@styles/CancelBookingModal.module.css';
+import { BiTrashAlt } from 'react-icons/bi';
+import constants from '../constants';
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+  booking: CancelBookingModalFragment$key;
+}
+
+const CancelBookingModalFragment = graphql`
+  fragment CancelBookingModalFragment on Booking {
+    id
+    start_date
+    service {
+      name
+    }
+  }
+`;
+
+const getConfirmationMessage = (data: any) => {
+  const startDate = new Date(data.start_date);
+  return (
+    <Text noOfLines={5} maxWidth={350}>
+      ¿Seguro que desea cancelar su reserva de <b>{data.service?.name}</b> del{' '}
+      <b>
+        {startDate.getDate()} de {constants.months[startDate.getMonth()]} de{' '}
+        {startDate.getFullYear()} a las {startDate.getHours()}:
+        {startDate.getMinutes()}
+      </b>
+      ?
+    </Text>
+  );
+};
+
+export default function CancelBookingModal({
+  isOpen,
+  onClose,
+  booking,
+}: Props): JSX.Element {
+  const data = useFragment(CancelBookingModalFragment, booking);
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay onClick={onClose} />
+      <ModalContent className={styles.modalContent}>
+        <ModalCloseButton color={'white'} />
+        <div className={styles.topBanner}>
+          <BiTrashAlt size={90} color={'white'} />
+        </div>
+        <div className={styles.confirmationMessageContainer}>
+          {getConfirmationMessage(data)}
+        </div>
+        <div className={styles.buttonsSectionContainer}>
+          <Button variant={'outline'} colorScheme={'gray'} onClick={onClose}>
+            Volver
+          </Button>
+          <Button colorScheme={'red'} onClick={onClose}>
+            Confirmar Cancelacion
+          </Button>
+        </div>
+      </ModalContent>
+    </Modal>
+  );
+}
--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,20 @@ const constants = {
   currentBookingEventTitle: 'Su reserva',
   placeholderImage:
     'https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image.png',
+  months: [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre',
+  ],
 };
 
 export default constants;
