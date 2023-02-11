3620c8115fc817a97006605f0faddc0fa3dd9d84
Nicolás Aguerre <naguerre@fi.uba.ar>
2023-02-11 20:32:19 -0300

    Finished incoming requests page

--- src/components/BookingCard.tsx
+++ src/components/BookingCard.tsx
@@ -11,7 +11,7 @@ import ServiceImage from './ServiceImage';
 import ServiceTags from './ServiceTags';
 import { graphql } from 'relay-runtime';
 import { BookingCardFragment$key } from './__generated__/BookingCardFragment.graphql';
-import { useFragment } from 'react-relay';
+import { useFragment, useMutation } from 'react-relay';
 import CancelBookingModal from './CancelBookingModal';
 
 interface Props {
@@ -71,7 +71,86 @@ const getFormattedDate = (date: Date): string => {
   const year = date.getFullYear();
   const hour = date.getHours();
   const minutes = date.getMinutes();
-  return `${day}/${month}/${year} a las ${hour}:${minutes}`;
+  return `${day}/${month}/${year} a las ${hour.toLocaleString('en-US', {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  })}:${minutes.toLocaleString('en-US', {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  })}`;
+};
+
+const ButtonGroup = ({
+  isPublisher,
+  bookingStatus,
+  onCancelClick,
+  onAcceptClick,
+  onRejectClick,
+  isMutationInFlight,
+}: {
+  isPublisher: boolean;
+  bookingStatus: string;
+  onCancelClick: () => void;
+  onAcceptClick: () => void;
+  onRejectClick: () => void;
+  isMutationInFlight: boolean;
+}): JSX.Element | null => {
+  if (isPublisher) {
+    switch (bookingStatus) {
+      case 'PENDING_CONFIRMATION':
+        return (
+          <>
+            <Button
+              colorScheme="green"
+              onClick={onAcceptClick}
+              className={styles.button}
+              isLoading={isMutationInFlight}
+              isDisabled={isMutationInFlight}
+            >
+              <IconWithText icon={<CheckIcon />} text={<p>Aceptar</p>} />
+            </Button>
+            <Button
+              colorScheme="red"
+              onClick={onRejectClick}
+              className={styles.button}
+              isLoading={isMutationInFlight}
+              isDisabled={isMutationInFlight}
+            >
+              <IconWithText icon={<CloseIcon />} text={<p>Rechazar</p>} />
+            </Button>
+          </>
+        );
+      case 'CONFIRMED':
+        return (
+          <Button
+            colorScheme="red"
+            onClick={onCancelClick}
+            className={styles.button}
+            isLoading={isMutationInFlight}
+            isDisabled={isMutationInFlight}
+          >
+            <IconWithText icon={<CloseIcon />} text={<p>Cancelar</p>} />
+          </Button>
+        );
+    }
+  } else {
+    switch (bookingStatus) {
+      case 'PENDING_CONFIRMATION':
+      case 'CONFIRMED':
+        return (
+          <Button
+            colorScheme="red"
+            onClick={onCancelClick}
+            className={styles.button}
+            isLoading={isMutationInFlight}
+            isDisabled={isMutationInFlight}
+          >
+            <IconWithText icon={<CloseIcon />} text={<p>Cancelar</p>} />
+          </Button>
+        );
+    }
+  }
+  return null;
 };
 
 export default function BookingCard({
@@ -82,6 +161,44 @@ export default function BookingCard({
   const data = useFragment(BookingCardFragment, booking);
   const startDate = new Date(data.start_date);
   const endDate = new Date(data.end_date);
+
+  const [commitMutation, isMutationInFlight] = useMutation(
+    graphql`
+      mutation BookingCardConfirmOrRejectMutation(
+        $booking_id: String!
+        $confirmed: Boolean!
+      ) {
+        acceptBooking(booking_id: $booking_id, accept: $confirmed) {
+          id
+          booking_status
+        }
+      }
+    `
+  );
+
+  const onAccept = () => {
+    commitMutation({
+      variables: {
+        booking_id: data.id,
+        confirmed: true,
+      },
+      onCompleted: data => {
+        console.log(data);
+      },
+    });
+  };
+
+  const onReject = () => {
+    commitMutation({
+      variables: {
+        booking_id: data.id,
+        confirmed: false,
+      },
+      onCompleted: data => {
+        console.log(data);
+      },
+    });
+  };
 
   return (
     <>
@@ -118,19 +235,14 @@ export default function BookingCard({
           </Stack>
         </div>
         <div className={styles.cancelBookingContainer}>
-          <Button
-            colorScheme="red"
-            disabled={data.booking_status === 'CANCELLED'}
-            className={styles.cancelButton}
-            onClick={onOpen}
-          >
-            <IconWithText icon={<CloseIcon />} text={<p>Cancelar</p>} />
-          </Button>
-          {isPublisher && data.booking_status === 'PENDING_CONFIRMATION' && (
-            <Button colorScheme="green" className={styles.cancelButton}>
-              <IconWithText icon={<CheckIcon />} text={<p>Aceptar</p>} />
-            </Button>
-          )}
+          <ButtonGroup
+            isPublisher={isPublisher}
+            bookingStatus={data.booking_status}
+            onCancelClick={onOpen}
+            onAcceptClick={onAccept}
+            onRejectClick={onReject}
+            isMutationInFlight={isMutationInFlight}
+          />
         </div>
       </div>
       <CancelBookingModal isOpen={isOpen} onClose={onClose} booking={data} />
