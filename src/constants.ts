537d4f14e506959e01094bafdfb1f46529545e29
santiago.klein <santiago.klein@mulesoft.com>
2023-02-11 12:19:19 -0300

    finish bookingModal functionality

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,9 @@
 const constants = {
   currentBookingEventTitle: 'Su reserva',
+  // TODO: review these values
+  existentBookingsQueryStartDate: '2000-01-01T00:00:00.000Z',
+  existentBookingsQueryEndDate: '9999-12-31T23:59:00.000Z',
+  existentBookingEventTitle: 'No disponible',
   placeholderImage:
     'https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image.png',
   months: [
