9bc67f26ce7bb96929e22b550178053a871691f1
Nicolás Aguerre <naguerre@fi.uba.ar>
2023-02-15 01:46:17 -0300

    Refetch myBookings after making a new booking

--- src/components/BookServiceModal.tsx
+++ src/components/BookServiceModal.tsx
@@ -139,6 +139,7 @@ export default function BookServiceModal({
       );
       onClose();
     },
+    refetchQueries: ['MyBookingsQuery'],
   });
 
   const { currentDate, initialFromDateString, initialToDateString } =
