c64cf16eba5470cc8c3e3a61bd9ffcc99fc8ffcd
santiago.klein <santiago.klein@mulesoft.com>
2023-02-15 20:39:45 -0300

    fix height overflow

--- src/components/Pages/HomePage/HomePage.tsx
+++ src/components/Pages/HomePage/HomePage.tsx
@@ -1,18 +1,21 @@
 import { NavigationBar, tabIndexToRouteArray } from './NavigationBar';
 import styles from '@styles/HomePage.module.css';
 import { useRouter } from 'found';
+import { Box } from '@chakra-ui/react';
 
 export default function HomePage(props: any): JSX.Element {
   const { match } = useRouter();
 
   return (
-    <div className={styles.pageContainer}>
-      <NavigationBar
-        defaultTabIndex={tabIndexToRouteArray.findIndex(
-          route => match.location.pathname === route
-        )}
-      />
-      <div className={styles.pageContent}>{props.children}</div>
-    </div>
+    <Box h="calc(100vh)">
+      <div className={styles.pageContainer}>
+        <NavigationBar
+          defaultTabIndex={tabIndexToRouteArray.findIndex(
+            route => match.location.pathname === route
+          )}
+        />
+        <div className={styles.pageContent}>{props.children}</div>
+      </div>
+    </Box>
   );
 }
