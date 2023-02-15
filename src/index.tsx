36450b74b80fdcd659510298d703f32490a9f50e
santiago.klein <santiago.klein@mulesoft.com>
2023-02-15 20:39:45 -0300

    fix height overflow

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
-import '@styles/index.module.css';
 import theme from './themes/default';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import {
