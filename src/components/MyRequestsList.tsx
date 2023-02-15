4325e7b994517decababbc3fdf20d40044446559
Nicolás Aguerre <naguerre@fi.uba.ar>
2023-02-15 02:06:43 -0300

    Populate requests with Apollo

--- src/components/MyRequestsList.tsx
+++ src/components/MyRequestsList.tsx
@@ -1,31 +1,49 @@
 import styles from '@styles/MyBookingsList.module.css';
 import BookingCard from './BookingCard';
+import { gql } from '../__generated__/gql';
+import { useQuery } from '@apollo/client';
+import { Spinner } from '@chakra-ui/spinner';
+import { Service } from '../__generated__/graphql';
+
+const myRequestsQuery = gql(/* GraphQL */ `
+  query MyRequests($cursor: String) {
+    myBookingsForPublisher(first: 10, after: $cursor) {
+      edges {
+        node {
+          id
+          booking_status
+          start_date
+          end_date
+          service {
+            name
+            description
+          }
+        }
+      }
+    }
+  }
+`);
 
 export default function MyRequestsList(): JSX.Element {
-  // const [isPending, startTransition] = useTransition();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  // const onLoadMore = () =>
-  //   startTransition(() => {
-  //     loadNext(3);
-  //   });
-  const data = {
-    myBookingsForPublisher: {
-      edges: [
-        {
-          node: {
-            id: 1,
-          },
-        },
-      ],
-    },
-  };
+  const { data, loading } = useQuery(myRequestsQuery);
+
+  if (loading || !data) {
+    return <Spinner />;
+  }
 
   return (
     <div className={styles.servicesContainer}>
       {data.myBookingsForPublisher.edges.length ? (
         data.myBookingsForPublisher.edges.map(booking => (
           <div key={booking.node.id} className={styles.cardContainer}>
-            <BookingCard isPublisher />
+            <BookingCard
+              isPublisher
+              bookingStatus={booking.node.booking_status}
+              startDate={booking.node.start_date}
+              endDate={booking.node.end_date}
+              id={booking.node.id}
+              service={booking.node.service as Service}
+            />
           </div>
         ))
       ) : (
