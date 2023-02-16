f05adc7d624db003c02c95c01395fcbfaa50a60f
Santiago Klein <38171261+sankle@users.noreply.github.com>
2023-02-15 23:21:30 -0300

    Expand page container as more services are loaded (#29)

--- src/components/Pages/HomePage/HomePage.tsx
+++ src/components/Pages/HomePage/HomePage.tsx
@@ -1,21 +1,18 @@
 import { NavigationBar, tabIndexToRouteArray } from './NavigationBar';
 import styles from '@styles/HomePage.module.css';
 import { useRouter } from 'found';
-import { Box } from '@chakra-ui/react';
 
 export default function HomePage(props: any): JSX.Element {
   const { match } = useRouter();
 
   return (
-    <Box h="calc(100vh)">
-      <div className={styles.pageContainer}>
-        <NavigationBar
-          defaultTabIndex={tabIndexToRouteArray.findIndex(
-            route => match.location.pathname === route
-          )}
-        />
-        <div className={styles.pageContent}>{props.children}</div>
-      </div>
-    </Box>
+    <div className={styles.pageContainer}>
+      <NavigationBar
+        defaultTabIndex={tabIndexToRouteArray.findIndex(
+          route => match.location.pathname === route
+        )}
+      />
+      <div className={styles.pageContent}>{props.children}</div>
+    </div>
   );
 }
